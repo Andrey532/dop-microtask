ab9a1e85ad66198085665aedf273e1e3e9890670
Andrii532 <andreygalushka30@gmail.com>
2022-10-14 23:35:03 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+type PropsType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+function App() {
+  const [todos, setTodos] = useState<Array<PropsType>>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
+  }, []);
+
+  const onClickHandler = () => {
+    setTodos([]);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickHandler}>CLEAN POSTS</button>
+      <ul>
+        {todos.map((el) => {
+          return (
+            <li>
+              <span>{el.id} - </span>
+              <span>{el.title}</span>
+              <span>{el.completed}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
